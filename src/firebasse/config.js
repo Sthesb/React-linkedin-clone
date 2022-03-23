c6b39f4a4056f9ad204614e8b52b6c660339d925
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-23 17:56:33 +0200

    Initial commit

---
+++ src/firebasse/config.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbY9oAzGvjkW-5WXIqFKRhjejzLWXDvzQ",
+    authDomain: "linkedin-clone-8783c.firebaseapp.com",
+    projectId: "linkedin-clone-8783c",
+    storageBucket: "linkedin-clone-8783c.appspot.com",
+    messagingSenderId: "101209661861",
+    appId: "1:101209661861:web:75dc5b7576c09ec4e9c84c"
+  };
